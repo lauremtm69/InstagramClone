2bc6f33687a3e8afcd2d33d42104b976feb28cba
lauremtm69 <alenmutum21@gmail.com>
2020-03-06 07:51:03 +0530

    calling a method with enter keyboard press hiding keyboard when screen is pressed, making text accept values without empty fields

--- app/src/main/java/com/alenmutum21/instagramclone/LoginActivity.java
+++ app/src/main/java/com/alenmutum21/instagramclone/LoginActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -14,6 +16,7 @@ import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class LoginActivity extends AppCompatActivity {
     private TextView noAccount;
@@ -28,10 +31,25 @@ public class LoginActivity extends AppCompatActivity {
 
         initfield();
 
+        pwdLogin.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                    loginUser();
+                }
+
+                return false;
+            }
+        });
+
+        if (ParseUser.getCurrentUser() != null){
+            ParseUser.getCurrentUser().logOut();
+        }
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginprogress.setVisibility(View.VISIBLE);
                 loginUser();
             }
         });
@@ -48,27 +66,46 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void loginUser() {
-        ParseUser.logInInBackground(emailLogin.getText().toString(), pwdLogin.getText().toString(), new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException e) {
-                if (user != null && e == null){
-                    Toast.makeText(LoginActivity.this, "login Succesfull", Toast.LENGTH_SHORT).show();
-                    loginprogress.setVisibility(View.GONE);
-                }else {
-                    Toast.makeText(LoginActivity.this, "error: "+ e.getMessage(), Toast.LENGTH_SHORT).show();
-                    loginprogress.setVisibility(View.GONE);
+        loginprogress.setVisibility(View.VISIBLE);
+        if (emailLogin.getText().toString().equals("") || pwdLogin.getText().toString().equals("")){
+
+            FancyToast.makeText(LoginActivity.this,"email and password field cannot be empty",FancyToast.LENGTH_LONG,FancyToast.INFO,false).show();
+            loginprogress.setVisibility(View.GONE);
+
+        }else {
+
+            ParseUser.logInInBackground(emailLogin.getText().toString(), pwdLogin.getText().toString(), new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (user != null && e == null) {
+                        FancyToast.makeText(LoginActivity.this, "login succesful for user " + ParseUser.getCurrentUser().getUsername(), FancyToast.LENGTH_LONG, FancyToast.SUCCESS, false).show();
+                        loginprogress.setVisibility(View.GONE);
+                    } else {
+                        FancyToast.makeText(LoginActivity.this, "error " + e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, false).show();
+                        loginprogress.setVisibility(View.GONE);
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 
     private void initfield() {
         getSupportActionBar().setTitle("Login page");
+        getSupportActionBar().setBackgroundDrawable(getDrawable(R.drawable.gradbtn));
         noAccount = findViewById(R.id.noAccount);
         login = findViewById(R.id.login);
         emailLogin = findViewById(R.id.emailLogin);
         pwdLogin = findViewById(R.id.pwdLogin);
         loginprogress = findViewById(R.id.loginProgress);
+
+    }
+    public void rootTapped(View view){
+        try {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
--- app/src/main/java/com/alenmutum21/instagramclone/MainActivity.java
+++ app/src/main/java/com/alenmutum21/instagramclone/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -16,6 +18,7 @@ import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView alreadyLink;
@@ -31,13 +34,29 @@ public class MainActivity extends AppCompatActivity {
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
         getSupportActionBar().setTitle("SignUp Page");
+        getSupportActionBar().setBackgroundDrawable(getDrawable(R.drawable.gradbtn));
 
         initFields();
+        password.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                    signUpUser();
+                }
+
+                return false;
+            }
+        });
+
+        if (ParseUser.getCurrentUser() != null){
+            ParseUser.getCurrentUser().logOut();
+        }
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadingSignUp.setVisibility(View.VISIBLE);
+
                 signUpUser();
             }
         });
@@ -54,26 +73,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void signUpUser() {
-        ParseUser parseUser = new ParseUser();
+        loadingSignUp.setVisibility(View.VISIBLE);
+        if (email.getText().toString().equals("") || username.getText().toString().equals("") || password.getText().toString().equals("")){
 
-        parseUser.setEmail(email.getText().toString());
-        parseUser.setUsername(username.getText().toString());
-        parseUser.setPassword(password.getText().toString());
+            FancyToast.makeText(MainActivity.this,"email, username, password is required ",FancyToast.LENGTH_LONG,FancyToast.INFO,false).show();
+            loadingSignUp.setVisibility(View.GONE);
 
-        parseUser.signUpInBackground(new SignUpCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null){
-                    loadingSignUp.setVisibility(View.GONE);
-                    Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+        }else {
 
-                }else {
-                    loadingSignUp.setVisibility(View.GONE);
-                    Toast.makeText(MainActivity.this, "error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            ParseUser parseUser = new ParseUser();
+
+            parseUser.setEmail(email.getText().toString());
+            parseUser.setUsername(username.getText().toString());
+            parseUser.setPassword(password.getText().toString());
+
+            parseUser.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        loadingSignUp.setVisibility(View.GONE);
+                        FancyToast.makeText(MainActivity.this, "SignUp succesful for user " + ParseUser.getCurrentUser().getUsername(), FancyToast.LENGTH_LONG, FancyToast.SUCCESS, false).show();
+
+
+                    } else {
+                        loadingSignUp.setVisibility(View.GONE);
+                        FancyToast.makeText(MainActivity.this, "error " + e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, false).show();
+
+                    }
+
                 }
-
-            }
-        });
+            });
+        }
     }
 
     private void initFields() {
@@ -84,5 +114,16 @@ public class MainActivity extends AppCompatActivity {
 
         signUp = findViewById(R.id.signUp);
         loadingSignUp = findViewById(R.id.loadingSignUp);
+    }
+
+    public void rootLayouttapped(View view){
+
+        try {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
